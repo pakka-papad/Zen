c65ddec9382e30e45b507222c55c62d9c931d929
pakka-papad <sumitzbera@gmail.com>
2022-09-21 00:35:28 +0530

    initial commit

---
+++ app/src/main/java/tech/zemn/mobile/MainActivity.kt
@@ -0,0 +1,17 @@
+package tech.zemn.mobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import tech.zemn.mobile.ui.theme.ZemnTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ZemnTheme {
+
+            }
+        }
+    }
+}
