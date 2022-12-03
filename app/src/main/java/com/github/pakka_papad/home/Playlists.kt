82870585ce6758d53fcd46c573fd7e63102a74d8
pakka-papad <sumitzbera@gmail.com>
2022-12-03 23:47:04 +0530

    Updated CreatePlaylistCard

--- app/src/main/java/com/github/pakka_papad/home/Playlists.kt
+++ app/src/main/java/com/github/pakka_papad/home/Playlists.kt
@@ -1,13 +1,17 @@
 package com.github.pakka_papad.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.github.pakka_papad.components.PlaylistCard
 import com.github.pakka_papad.data.music.Playlist
@@ -47,27 +51,23 @@ fun Playlists(
 fun CreatePlaylistCard(
     onPlaylistCreate: (String) -> Unit
 ) {
+    var isDialogVisible by remember { mutableStateOf(false) }
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .height(80.dp)
             .padding(12.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .background(MaterialTheme.colorScheme.primary)
+            .clickable { isDialogVisible = true },
+        contentAlignment = Alignment.Center
     ) {
-        var isDialogVisible by remember { mutableStateOf(false) }
         var playlistName by remember { mutableStateOf("") }
-        Button(
-            onClick = {
-                isDialogVisible = true
-            },
-            modifier = Modifier
-                .fillMaxHeight()
-                .align(Alignment.Center)
-        ) {
-            Text(
-                text = "Create new playlist",
-                style = MaterialTheme.typography.bodyLarge
-            )
-        }
+        Text(
+            text = "Create new playlist",
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
         if (isDialogVisible) {
             AlertDialog(
                 onDismissRequest = { isDialogVisible = false },
@@ -79,18 +79,40 @@ fun CreatePlaylistCard(
                             playlistName = ""
                         }
                     ) {
-                        Text(text = "OK")
+                        Text(
+                            text = "Create",
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+                },
+                dismissButton = {
+                    OutlinedButton(
+                        onClick = {
+                            isDialogVisible = false
+                            playlistName = ""
+                        }
+                    ){
+                        Text(
+                            text = "Cancel",
+                            style = MaterialTheme.typography.bodyLarge
+                        )
                     }
                 },
                 title = {
-                    Text(text = "Playlist name")
+                    Text(
+                        text = "Create playlist"
+                    )
                 },
                 text = {
-                    TextField(
+                    OutlinedTextField(
                         value = playlistName,
                         onValueChange = {
                             playlistName = it
                         },
+                        label = {
+                            Text(text = "Playlist name")
+                        },
+                        textStyle = MaterialTheme.typography.bodyLarge
                     )
                 }
             )
