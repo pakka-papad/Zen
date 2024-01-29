3910991d6689094f69a585f4cd5625ab7fda1266
pakka-papad <76241334+pakka-papad@users.noreply.github.com>
2024-01-29 17:16:37 +0530

    Updated: Version change

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -147,7 +147,7 @@ object AnnotationProcessors {
 object AppVersion {
     private const val Major = 1
     private const val Minor = 2
-    private const val Patch = 2
+    private const val Patch = 3
     const val Code = Major*10000 + Minor*100 + Patch
     const val Name = "$Major.$Minor.$Patch"
 }
